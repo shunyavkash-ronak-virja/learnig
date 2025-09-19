440b097942566da5b970f627ba4a9ee454c19cf1
shunyavkash-ronak-virja <ronak.virja@shunyavkash.com>
2025-09-19 17:38:08 +0530

    create splide

--- 12-september/assets/js/script.js
+++ 12-september/assets/js/script.js
@@ -1,7 +1,21 @@
+// Navbar js
 const navbarToggle = document.querySelector(".nav-toggle");
 const navbarMenu = document.querySelector(".nav-links-wrapper");
 
 navbarToggle.addEventListener("click", () => {
   navbarToggle.classList.toggle("active");
   navbarMenu.classList.toggle("active");
+});
+
+// Splide js
+document.addEventListener("DOMContentLoaded", function () {
+  var splide = new Splide(".splide", {
+    // type: "loop",
+    // autoplay: "true",
+    // pauseOnHover: "true",
+    // perPage: 3,
+    width: "70%",
+    // gap: 16pageXOffset;
+  });
+  splide.mount();
 });
