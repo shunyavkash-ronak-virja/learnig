100153e618dae47dd2af525497b79647b6852d74
shunyavkash-ronak-virja <ronak.virja@shunyavkash.com>
2025-10-08 18:37:34 +0530

    created navbar

---
+++ mclaw-project/assets/js/script.js
@@ -0,0 +1,7 @@
+// Nav toggle - - - - - - - - - -
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelector(".nav-links-block");
+navToggle.addEventListener("click", () => {
+  navToggle.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
