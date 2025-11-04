81d67921e20ef9a4048ff3a48d2e7618cb016025
shunyavkash-ronak-virja <ronak.virja@shunyavkash.com>
2025-11-04 14:13:54 +0530

    created responsive header

--- figma-demo/assets/js/script.js
+++ figma-demo/assets/js/script.js
@@ -1,13 +1,7 @@
-const navToggle = document.querySelectorAll(".toggle-overlay");
-const navLinks = document.querySelectorAll(".header-link-container");
-navToggle.addEventListener("click", () => {
-  navToggle.classList.toggle(".active");
-  navLinks.classList.toggle(".active");
-});
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelector(".header-link-container");
 
-// const navbarToggle = document.querySelector(".nav-toggle");
-// const navbarMenu = document.querySelector(".nav-links");
-// navbarToggle.addEventListener("click", () => {
-//   navbarToggle.classList.toggle("active");
-//   navbarMenu.classList.toggle("active");
-// });
+navToggle.addEventListener("click", () => {
+  navToggle.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
