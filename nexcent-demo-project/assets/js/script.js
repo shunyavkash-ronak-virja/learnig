b21aaecce54a83e3a4c3caa836757b2040f1ec96
shunyavkash-ronak-virja <ronak.virja@shunyavkash.com>
2025-11-20 16:09:24 +0530

    redesign section and update css

--- nexcent-demo-project/assets/js/script.js
+++ nexcent-demo-project/assets/js/script.js
@@ -7,14 +7,27 @@ navToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
-//Splide js
+// Hero Splide js
 document.addEventListener("DOMContentLoaded", function () {
   var splide = new Splide(".hero-slider", {
     type: "loop",
-    // autoplay: true,
+    autoplay: true,
     pauseOnFocus: true,
     perPage: 1,
     arrows: false,
   });
   splide.mount();
 });
+
+// Our-clients Splide js
+if (document.querySelector(".our-clients-slider.splide"))
+  new Splide(".our-clients-slider", {
+    type: "loop",
+    // autoplay: true,
+    gap: 80,
+    pagination: !1,
+    arrows: !1,
+    perPage: !7,
+    autoWidth: !0,
+    autoScroll: { speed: 0.7 },
+  }).mount(window.splide.Extensions);
