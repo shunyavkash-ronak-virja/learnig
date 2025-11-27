adb5e5ef1c6d51273353619b7c03ef44fec9c674
shunyavkash-ronak-virja <ronak.virja@shunyavkash.com>
2025-11-27 18:15:47 +0530

    changes : remove unused css and make perfect

--- animation-slider/assets/js/script.js
+++ animation-slider/assets/js/script.js
@@ -1,40 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Main slider
-  var main = new Splide(".splide", {
+  var main = new Splide(".fruit-slider.splide", {
     type: "fade",
-    autoplay: true,
+    // autoplay: true,
     perPage: 1,
     pagination: false,
-    cover: true,
     drag: false,
     interval: 4500,
     arrows: false,
   });
 
   // Thumbnail slider
-  var thumbnails = new Splide("#thumbnails", {
+  var thumbnails = new Splide(".thumbnails-slider", {
     type: "loop",
     drag: false,
-    autoplay: true,
-    rewind: true,
-    fixedWidth: 104,
-    fixedHeight: 58,
+    // autoplay: true,
     isNavigation: true,
     perPage: 2,
-    gap: 10,
+    gap: 20,
     pagination: false,
-    cover: true,
-    dragMinThreshold: {
-      mouse: 4,
-      touch: 10,
-    },
     breakpoints: {
       1023: {
         perPage: 1,
-      },
-      640: {
-        fixedWidth: 66,
-        fixedHeight: 38,
       },
     },
   });
