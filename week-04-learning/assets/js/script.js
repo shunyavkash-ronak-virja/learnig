701919f07b32eb06706ef91cd8538c79cd74227a
shunyavkash-ronak-virja <ronak.virja@shunyavkash.com>
2025-12-02 14:58:58 +0530

    change header design

--- week-04-learning/assets/js/script.js
+++ week-04-learning/assets/js/script.js
@@ -5,3 +5,12 @@ navToggle.addEventListener("click", () => {
   navToggle.classList.toggle("active");
   navLinks.classList.toggle("active");
 });
+
+const menuStore = document.querySelectorAll(".submenu-wrapper");
+// const subMenu = document.querySelectorAll(".link-dropdown-container");
+
+menuStore.forEach((val) => {
+  val.addEventListener("click", (e) => {
+    val.classList.toggle("visible");
+  });
+});
