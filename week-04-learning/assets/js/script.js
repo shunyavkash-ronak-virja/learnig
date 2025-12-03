4cb3e8c0ef86e17da6bb1b619c60c76cbba591ca
shunyavkash-ronak-virja <ronak.virja@shunyavkash.com>
2025-12-03 14:29:31 +0530

    created header

--- week-04-learning/assets/js/script.js
+++ week-04-learning/assets/js/script.js
@@ -1,11 +1,18 @@
+// header toggle js
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelector(".header-content-container");
-
+const hidden = document.querySelector("body");
+window.addEventListener("resize", () => {
+  navLinks.classList.remove("active");
+  navToggle.classList.remove("active");
+});
 navToggle.addEventListener("click", () => {
   navToggle.classList.toggle("active");
   navLinks.classList.toggle("active");
+  hidden.classList.toggle("hidden");
 });
 
+// submenu click js
 if (window.innerWidth <= "991") {
   const menuStore = document.querySelectorAll(".submenu-wrapper");
   menuStore.forEach((val) => {
