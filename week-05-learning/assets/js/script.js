08655c1a085969e8b2cc90686e7aa4088c4cb679
shunyavkash-ronak-virja <ronak.virja@shunyavkash.com>
2025-12-03 19:03:47 +0530

    added js

--- week-05-learning/assets/js/script.js
+++ week-05-learning/assets/js/script.js
@@ -1,7 +1,11 @@
 const toggleBtn = document.querySelector(".sidebar-btn");
+const sideBar = document.querySelector(".sidebar-container");
 const closeBtn = document.querySelector(".sidebar-close-btn");
 
 toggleBtn.addEventListener("click", () => {
-  toggleBtn.classList.add("active");
-  closeBtn.classList.remove("active");
+  sideBar.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  sideBar.classList.remove("active");
 });
