3c98201b340ac7314f98c9e8cc77cb196715f5d5
shunyavkash-ronak-virja <ronak.virja@shunyavkash.com>
2025-12-05 18:38:54 +0530

    added js

--- pricing-card/assets/js/script.js
+++ pricing-card/assets/js/script.js
@@ -1,12 +1,18 @@
 const monthlyPricing = document.querySelector(".monthly");
 const yearlyPricing = document.querySelector(".yearly");
+const monthlyCard = document.querySelector(".monthly-card-wrapper");
+const yearlyCard = document.querySelector(".yearly-card-wrapper");
 
 yearlyPricing.addEventListener("click", () => {
   monthlyPricing.classList.remove("active");
   yearlyPricing.classList.add("active");
+  monthlyCard.style.display = "none";
+  yearlyCard.style.display = "grid";
 });
 
 monthlyPricing.addEventListener("click", () => {
   monthlyPricing.classList.add("active");
   yearlyPricing.classList.remove("active");
+  yearlyCard.style.display = "none";
+  monthlyCard.style.display = "grid";
 });
