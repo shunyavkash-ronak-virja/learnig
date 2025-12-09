b3b2950d6b3ba946fd7b8458bd64391314c8491d
shunyavkash-ronak-virja <ronak.virja@shunyavkash.com>
2025-12-09 11:04:48 +0530

    made complete improvement

--- week-04-learning/assets/js/script.js
+++ week-04-learning/assets/js/script.js
@@ -1,22 +1,32 @@
-// header toggle js
+// header toggle js ----------
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelector(".header-content-container");
 const hidden = document.querySelector("body");
+
 window.addEventListener("resize", () => {
   navLinks.classList.remove("active");
   navToggle.classList.remove("active");
   hidden.classList.remove("hidden");
 });
+
 navToggle.addEventListener("click", () => {
   navToggle.classList.toggle("active");
   navLinks.classList.toggle("active");
   hidden.classList.toggle("hidden");
 });
 
-// submenu click js
+// submenu click js ----------
 const menuStore = document.querySelectorAll(".submenu-wrapper");
 menuStore.forEach((val) => {
   val.addEventListener("click", (e) => {
     val.classList.toggle("visible");
   });
 });
+
+// Header hight ----------
+const headerElement = document.querySelector(".header");
+const headerHeight = headerElement.offsetHeight + 16;
+document.documentElement.style.setProperty(
+  "--header-height",
+  `${headerHeight}px`
+);
