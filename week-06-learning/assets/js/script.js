7038a594a0d64130d2d228b443c18d72d098b630
shunyavkash-ronak-virja <ronak.virja@shunyavkash.com>
2025-12-16 14:08:35 +0530

    improvements : header and footer

--- week-06-learning/assets/js/script.js
+++ week-06-learning/assets/js/script.js
@@ -1,8 +1,12 @@
 // Header toggle -----
 const headerToggle = document.querySelector(".header-toggle");
 const headerWrapper = document.querySelector(".heaer-contents-container");
+const header = document.querySelector(".header");
+const bodyHidden = document.querySelector("body");
 
 headerToggle.addEventListener("click", () => {
   headerToggle.classList.toggle("active");
   headerWrapper.classList.toggle("active");
+  header.classList.toggle("active");
+  bodyHidden.classList.toggle("hiddden-overflow");
 });
