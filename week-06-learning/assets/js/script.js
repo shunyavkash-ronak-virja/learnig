60e4f8907885524c0549491c5f0fe749fd4dec98
shunyavkash-ronak-virja <ronak.virja@shunyavkash.com>
2025-12-17 11:36:12 +0530

    created hero section

--- week-06-learning/assets/js/script.js
+++ week-06-learning/assets/js/script.js
@@ -7,6 +7,16 @@ const bodyHidden = document.querySelector("body");
 headerToggle.addEventListener("click", () => {
   headerToggle.classList.toggle("active");
   headerWrapper.classList.toggle("active");
-  header.classList.toggle("active");
+  header.classList.toggle("header-bg");
   bodyHidden.classList.toggle("hiddden-overflow");
+});
+
+// Header scroll event
+const headerHeight = document.querySelector(".header");
+addEventListener("scroll", () => {
+  if (window.pageYOffset > 50) {
+    headerHeight.classList.add("header-scroll");
+  } else {
+    headerHeight.classList.remove("header-scroll");
+  }
 });
