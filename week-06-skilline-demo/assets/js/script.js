b4dfd4f0a15b802f06bfe6d9f53c6fca75b0a821
shunyavkash-ronak-virja <ronak.virja@shunyavkash.com>
2025-12-25 19:02:07 +0530

    created theme and header in skilline project

---
+++ week-06-skilline-demo/assets/js/script.js
@@ -0,0 +1,11 @@
+const navToggle = document.querySelector(".nav-toggle");
+const headerList = document.querySelector(".header-link-container");
+const header = document.querySelector(".header");
+// const bodyScroll = document.querySelector("body");
+
+navToggle.addEventListener("click", () => {
+  navToggle.classList.toggle("active");
+  headerList.classList.toggle("active");
+  header.classList.toggle("active");
+  //   bodyScroll.classList.toggle("body-scroll");
+});
