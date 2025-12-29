90b413bd177bc4239967398824d244c1b9a06350
shunyavkash-ronak-virja <ronak.virja@shunyavkash.com>
2025-12-29 19:08:49 +0530

    created : services, blog card and video column sections

--- week-06-skilline-demo/assets/js/script.js
+++ week-06-skilline-demo/assets/js/script.js
@@ -1,13 +1,23 @@
 const navToggle = document.querySelector(".nav-toggle");
 const headerList = document.querySelector(".header-link-container");
 const header = document.querySelector(".header");
-// const bodyScroll = document.querySelector("body");
 
 navToggle.addEventListener("click", () => {
   navToggle.classList.toggle("active");
   headerList.classList.toggle("active");
   header.classList.toggle("active");
-  //   bodyScroll.classList.toggle("body-scroll");
+});
+
+// Scroll js
+document.addEventListener("DOMContentLoaded", function () {
+  const headerHeight = document.querySelector(".header");
+  addEventListener("scroll", () => {
+    if (window.pageYOffset > 30) {
+      headerHeight.classList.add("body-scroll");
+    } else {
+      headerHeight.classList.remove("body-scroll");
+    }
+  });
 });
 
 // Fancybox js -----
