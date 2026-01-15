748727a0490f44f1d87f2aaf1e7e31bb7e2162b6
Ronak Virja <ronak.virja@shunyavkash.com>
2026-01-15 18:31:26 +0530

    created : articles slider and our story section

--- makefast-demo/assets/js/script.js
+++ makefast-demo/assets/js/script.js
@@ -19,12 +19,46 @@ document.addEventListener("DOMContentLoaded", function () {
     perPage: 1,
     perMove: 1,
     arrows: false,
-    interval: 5000,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    rewind: true,
     gap: 40,
+    interval: 5000,
+    speed: 1200,
+    easing: "ease-in-out",
 
     breakpoints: {
       767: {
         pagination: false,
+      },
+    },
+  });
+  splide.mount();
+});
+
+// Scroll header js
+document.addEventListener("DOMContentLoaded", function () {
+  const headerWrapper = document.querySelector("header");
+  addEventListener("scroll", () => {
+    if (window.pageYOffset > 30) {
+      headerWrapper.classList.add("body-scroll");
+    } else {
+      headerWrapper.classList.remove("body-scroll");
+    }
+  });
+});
+
+// Articles splide js
+document.addEventListener("DOMContentLoaded", function () {
+  var splide = new Splide(".articles-splide.splide", {
+    type: "slide",
+    autoWidth: true,
+    perMove: 3,
+    pagination: false,
+    gap: 26,
+    breakpoints: {
+      767: {
+        arrows: false,
       },
     },
   });
