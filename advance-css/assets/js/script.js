7e599d1551fcf1003f3c6a656faf2a776a4fbc25
Ronak Virja <ronak.virja@shunyavkash.com>
2026-03-25 18:32:31 +0530

    added scroll js and update header js

--- advance-css/assets/js/script.js
+++ advance-css/assets/js/script.js
@@ -1,10 +1,12 @@
 const header = document.getElementsByTagName("header");
 if (header.length) {
+  const header = document.querySelector("header");
   const toggleBtn = document.querySelector(".toggle-btn");
   const headerLinksWrapper = document.querySelector(".header-links-wrapper");
   const dropDownContent = document.querySelectorAll(".drop-down-content");
 
   toggleBtn.addEventListener("click", () => {
+    header.classList.toggle("active");
     toggleBtn.classList.toggle("active");
     headerLinksWrapper.classList.toggle("active");
   });
@@ -15,3 +17,15 @@ if (header.length) {
     });
   });
 }
+
+// Scroll js -----
+document.addEventListener("DOMContentLoaded", function () {
+  const headerHeight = document.querySelector("header");
+  addEventListener("scroll", () => {
+    if (window.pageYOffset > 30) {
+      headerHeight.classList.add("body-scroll");
+    } else {
+      headerHeight.classList.remove("body-scroll");
+    }
+  });
+});
